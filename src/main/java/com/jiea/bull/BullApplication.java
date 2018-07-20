1abf463441da01e9e00ac133f0cc15846e58a574
jiea <email_lijie@sina.com>
2018-07-20 14:09:22 +0800

    first commit

---
+++ src/main/java/com/jiea/bull/BullApplication.java
@@ -0,0 +1,12 @@
+package com.jiea.bull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BullApplication.class, args);
+    }
+}
