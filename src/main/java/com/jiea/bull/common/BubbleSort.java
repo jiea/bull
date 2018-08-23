7ef95f0c02350cdb92609675a01cadb0fdbdcc66
jiea <email_lijie@sina.com>
2018-08-23 19:11:05 +0800

    sss

---
+++ src/main/java/com/jiea/bull/common/BubbleSort.java
@@ -0,0 +1,23 @@
+package com.jiea.bull.common;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] array = {5, 8, 6, 3, 9, 2, 1, 7};
+
+        int a = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    a = array[i];
+                    array[i] = array[j];
+                    array[j] = a;
+                }
+            }
+
+        }
+        for (int k = 0; k < array.length; k++) {
+            System.out.print(array[k] + ",");
+        }
+    }
+}
